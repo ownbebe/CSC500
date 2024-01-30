a08b85663c40626a4a39a31893fbca3695526fb7
oSchneider <owen.schneider@csuglobal.edu>
2024-01-30 15:21:53 -0700

    Combined files and made small updates

--- MealCalculator/MealAndAlarm.py
+++ MealCalculator/MealAndAlarm.py
@@ -41,9 +41,40 @@ def validateInputCurrent(value):
 def validateInputTimer(value):
     if value < 0:
         raise InvalidInputError(f'Input must be greater than 0, not {value} ')
+    
+    
+class MealCalc:
+    total = 0
+    charge = 0
+    tip = 0.18
+    salesTax = 0.07
+
+    def __init__(self, charge):
+        self.charge = charge
+
+    def calculateTotal(self):
+        tipTotal = 0
+        taxTotal = 0
+        self.total = (self.total + self.charge)
+
+        taxTotal = self.total * self.salesTax
+        print(f'The total for tax is: $' + str(round(taxTotal, 2)))
+
+        self.total += taxTotal
+        print(f'The total for the meal with tax is: $' + str(round(self.total, 2)))
+
+        tipTotal = self.total * self.tip
+        print(f'The total for the tip is: $' + str(round(tipTotal, 2)))
+
+        self.total += tipTotal
+        print(f'The total for the meal, tip, and tax is: $' + str(round(self.total, 2)))
 
 class Main:       
     def __init__(self):
+        self.mealCost = self.getMealCost()
+        self.meal = MealCalc(self.mealCost)
+        self.meal.calculateTotal()
+        
         self.current = self.getCurrent()
         self.timer = self.getTimer()
         self.alarm = Alarm(self.timer, self.current)
@@ -71,6 +102,17 @@ class Main:
                 print(e.message)
                 
         return timer
+    
+    def getMealCost(self):
+        while True:
+            try:
+                mealCost = float(input(f'How much did your meal cost: '))
+                validateInputTimer(mealCost)
+                break
+            except InvalidInputError as e:
+                print(e.message)
+                
+        return mealCost
 
 if __name__ == "__main__": 
     Main()
