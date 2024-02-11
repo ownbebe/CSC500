d6e9cd3b62c7245c77a4c91fa218dc27ef053aee
oSchneider <owen.schneider@csuglobal.edu>
2024-02-10 23:16:23 -0700

    Portfolio project milestone 1

---
+++ PortfolioMilestone/ItemToPurchase.py
@@ -0,0 +1,17 @@
+class ItemToPurchase:
+    itemName = "none"
+    itemPrice = 0.00
+    itemQuantity = 0
+    
+    def __init__(self, itemName, itemPrice, itemQuantity):
+        self.itemName = itemName
+        self.itemPrice = itemPrice
+        self.itemQuantity = itemQuantity
+        
+    def itemTotal(self):
+        return self.itemPrice * self.itemQuantity
+        
+    def print_item_cost(self):
+        return f'{self.itemName} {self.itemQuantity} @ ${self.itemPrice:.2f} = ${self.itemTotal():.2f}' 
+    
+    
---
+++ PortfolioMilestone/Main.py
@@ -0,0 +1,66 @@
+import ItemToPurchase as ITP 
+
+class InvalidInputError(Exception):
+    def __init__(self, message):
+        self.message = message
+def validateInput(value):
+    if value < 0:
+        raise InvalidInputError(f'Input must be greater than 0, not {value} ')
+def validateString(value):
+    if "".__eq__(value):
+        raise InvalidInputError(f'You must enter a string')
+
+class Maintenance:
+    def __init__(self) -> None:
+        pass
+    
+    def itemInput(self):
+        while True:
+            try:
+                itemName = str(input("Enter the name of the item: "))
+                validateString(itemName)
+                break
+            except InvalidInputError as e:
+                    print(e.message)
+                    
+        while True:
+            try:
+                itemPrice = float(input("Enther the price of the item: "))
+                validateInput(itemPrice)
+                break
+            except InvalidInputError as e:
+                    print(e.message)
+                    
+        while True:
+            try:
+                itemQuantity = int(input("Enter the quantity being purchased: "))
+                validateInput(itemQuantity)
+                break
+            except InvalidInputError as e:
+                    print(e.message)
+        
+        
+        
+        item = ITP.ItemToPurchase(itemName, itemPrice, itemQuantity)
+        
+        return item
+    
+    def display(self):
+        print(f'Item 1')
+        item1 = self.itemInput()
+        
+        print(f'Item 2')
+        item2 = self.itemInput()
+        
+        totalCost = item1.itemTotal() + item2.itemTotal()
+        print(f'TOTAL COST \n{item1.print_item_cost()} \n{item2.print_item_cost()}\n Total: ${totalCost:.2f}')
+        
+class Main: 
+    def __init__(self):
+        self.Maintenance = Maintenance()
+        self.Maintenance.display()
+        
+        
+        
+if __name__ == "__main__":
+    Main()       
